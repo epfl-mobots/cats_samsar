c729548fcddf245c666a88103dca3bbb882e908f
msalamin <mickael.salamin@epfl.ch>
2019-10-24 11:16:28 +0200

    Remove commented template implementations from cpp
    Keep template methods implementations in hpp files only
    and remove the commented versions from the related cpp files.

--- source/robot-control/navigation/elastic-band/HomotopyClassPlanner.cpp
+++ source/robot-control/navigation/elastic-band/HomotopyClassPlanner.cpp
@@ -353,32 +353,6 @@ void HomotopyClassPlanner::exploreEquivalenceClassesAndInitTebs(const PoseSE2& s
 }
 
 
-// template<typename BidirIter, typename Fun>
-// TebPlannerPtr HomotopyClassPlanner::addAndInitNewTeb(BidirIter path_start, BidirIter path_end, Fun fun_position, double start_orientation, double goal_orientation, const geometry_msgs::Twist* start_velocity)
-// {
-//   TebPlannerPtr candidate = TebPlannerPtr( new TebPlanner(*cfg_, obstacles_, robot_model_));
-
-//   candidate->teb().initTrajectoryToGoal(path_start, path_end, fun_position, cfg_->robot.max_vel_x, cfg_->robot.max_vel_theta,
-//                                  cfg_->robot.acc_lim_x, cfg_->robot.acc_lim_theta, start_orientation, goal_orientation, cfg_->trajectory.min_samples,
-//                                  cfg_->trajectory.allow_init_with_backwards_motion);
-
-//   if (start_velocity)
-//     candidate->setVelocityStart(*start_velocity);
-
-//   EquivalenceClassPtr H = calculateEquivalenceClass(candidate->teb().poses().begin(), candidate->teb().poses().end(), getCplxFromVertexPosePtr, obstacles_,
-//                                                     candidate->teb().timediffs().begin(), candidate->teb().timediffs().end());
-
-//   if(addEquivalenceClassIfNew(H))
-//   {
-//     tebs_.push_back(candidate);
-//     return tebs_.back();
-//   }
-
-//   // If the candidate constitutes no new equivalence class, return a null pointer
-//   return TebPlannerPtr();
-// }
-
-
 TebPlannerPtr HomotopyClassPlanner::addAndInitNewTeb(const PoseSE2& start, const PoseSE2& goal, const geometry_msgs::Twist* start_velocity)
 {
   if(tebs_.size() >= cfg_->hcp.max_number_classes)
@@ -717,24 +691,6 @@ bool HomotopyClassPlanner::isHorizonReductionAppropriate(const std::vector<geome
 
   return best->isHorizonReductionAppropriate(initial_plan);
 }
-
-// template<typename BidirIter, typename Fun>
-// EquivalenceClassPtr HomotopyClassPlanner::calculateEquivalenceClass(BidirIter path_start, BidirIter path_end, Fun fun_cplx_point, const ObstacleContainer* obstacles,
-//                                                                     boost::optional<TimeDiffSequence::iterator> timediff_start, boost::optional<TimeDiffSequence::iterator> timediff_end)
-// {
-//   if(cfg_->obstacles.include_dynamic_obstacles)
-//   {
-//     HSignature3d* H = new HSignature3d(*cfg_);
-//     H->calculateHSignature(path_start, path_end, fun_cplx_point, obstacles, timediff_start, timediff_end);
-//     return EquivalenceClassPtr(H);
-//   }
-//   else
-//   {
-//     HSignature* H = new HSignature(*cfg_);
-//     H->calculateHSignature(path_start, path_end, fun_cplx_point, obstacles);
-//     return EquivalenceClassPtr(H);
-//   }
-// }
 
 void HomotopyClassPlanner::computeCurrentCost(std::vector<double>& cost, double obst_cost_scale, double viapoint_cost_scale, bool alternative_time_cost)
 {
